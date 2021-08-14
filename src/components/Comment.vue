68356a23206f5235a77e8928c79e3d209abf6ba2
waleed alqallaf <waleedalqalaf2@gmail.com>
2021-08-14 14:16:37 +0300

    Graphics for added and remove issue labels

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,16 +1,49 @@
 <template>
   <div class="card">
-    <div v-if="commentData.author != null" class="card-header">
-      {{commentData.author.username}} <span class="text-muted"> commented at {{creationDate}} </span>
+    <div v-if="commentData.author != null && !commentData.is_status_change" class="card-header">
+      {{commentData.author.username}} 
+      <strong class="text-primary"> (Logged In) </strong> 
+      <span class="text-muted"> commented at {{creationDate}} </span>
     </div>
-    <div v-else class="card-header">
-      {{commentData.guest_name}} <span class="text-muted"> commented at {{creationDate}} </span>
+    <div v-else-if="!commentData.is_status_change" class="card-header">
+      {{commentData.guest_name}} 
+      <strong class="text-secondary"> (Anonymous) </strong> 
+      <span class="text-muted"> commented at {{creationDate}} </span>
     </div>
     <div class="card-body">
       
-      <div class="card-text">
+      <div v-if="!commentData.is_status_change" class="card-text">
         {{commentData.body}}
       </div>
+      <div v-else class="card-text">
+        <div v-if="statusBodyData.type=='label'">
+          <strong> {{commentData.author.username}} </strong>
+          <span v-if="statusBodyData.action=='removed'">
+            Removed Label 
+          </span>
+          <span v-if="statusBodyData.action=='added'">
+            Added Label 
+          </span>
+          <IssueLabel
+              :label="statusBodyData.label.name"
+              :color="statusBodyData.label.color"
+              :noCheck="true"/>
+          at
+          <span class="text-muted"> {{creationDate}} </span>
+        </div>
+        <div v-else-if="statusBodyData.type=='issue'">
+          <strong> {{commentData.author.username}} </strong>
+          <span v-if="statusBodyData.action=='open'">
+            Opened Issue 
+          </span>
+          <span v-if="statusBodyData.action=='close'">
+            Closed Issue 
+          </span>
+          at
+          <span class="text-muted"> {{creationDate}} </span>
+        </div>
+      </div>
+
     </div>
     
   </div>
@@ -19,23 +52,32 @@
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import { format_django_date } from '@/globals/date';
+import IssueLabel from './IssueLabel.vue';
 
 @Options({
   props: {
     commentData: Object
   },
+  components: {
+    IssueLabel
+  },
   data() {
     return {
-      creationDate: ""
+      creationDate: "",
+      statusBodyData: {}
     }
   }
 })
 export default class Comment extends Vue {
   commentData!: any;
   creationDate!: string;
+  statusBodyData!: any;
 
   mounted() {
-    this.creationDate = format_django_date(this.commentData.creation_date)
+    this.creationDate = format_django_date(this.commentData.creation_date);
+
+    if(this.commentData.is_status_change)
+      this.statusBodyData = JSON.parse(this.commentData.body);
   }
 }
 </script>
