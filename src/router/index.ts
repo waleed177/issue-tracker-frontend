f56c605fe3e074e3d67cad7fb95102c9519a16b3
waleed alqallaf <waleedalqalaf2@gmail.com>
2021-08-16 18:18:35 +0300

    fixed indentation in preamble

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,8 +12,8 @@
 //   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //   GNU Affero General Public License for more details.
 //
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//   You should have received a copy of the GNU Affero General Public License
+//   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //#endregion
 
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
